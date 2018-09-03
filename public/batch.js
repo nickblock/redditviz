4360a6a1c7dad5333cf1c8aaac2f2674a8066cdf
Nick Block <nickblock@hotmail.com>
2018-09-03 17:53:58 +0100

    render orb data

--- public/batch.js
+++ public/batch.js
@@ -7,7 +7,41 @@
  the command is executed once for each object in the array. </p>
 */
 
+
+var OrbManager = function() {
+    
+    this.orbs = [];
+}
+OrbManager.prototype = {
+
+    create: function(data) {
+        
+        this.orbs = [];
+        var labels = [];
+        var count = data.length < chart_item_max ? data.length : chart_item_max;
+        for(var i=1; i<count; i++) {
+            this.orbs.push({
+                x: i,
+                y: Math.floor(Math.random() * 10),
+                r: data[i].count,
+                l: data[i].name
+            });
+        }
+    },
+    render: function() {
+        var drawArray = [];
+        for(var i=0; i<this.orbs.length; i++) {
+            var orb = this.orbs[i];
+            drawArray.push({offset:[orb.x, orb.y], scale:orb.r});
+        }
+        return drawArray;
+    }
+}
+
+var TheOrbManager = new OrbManager();
+
 var circlePoints = 200;
+var screenSize = [1.0, 1.0]
 
 var genCircleAttributes = function(r, p) {
     var attributes = [0.0, 0.0];
@@ -44,8 +78,11 @@ const draw = regl({
     attribute vec2 position;
     uniform float scale;
     uniform vec2 offset;
+    uniform vec2 screenSize;
     void main() {
-      gl_Position = vec4((vec2(position.x, position.y) * scale) + offset, 0.0, 1.0);
+        vec2 pos = (vec2(position.x, position.y) * scale) + offset;
+        pos = pos / screenSize;
+      gl_Position = vec4(pos, 0.0, 1.0);
     }`,
 
   attributes: {
@@ -63,7 +100,8 @@ const draw = regl({
       1
     ],
     offset: regl.prop('offset'),
-    scale: regl.prop('scale')
+    scale: regl.prop('scale'),
+    screenSize: screenSize
   },
 
   depth: {
@@ -80,17 +118,7 @@ regl.frame(function () {
   })
 
   // This tells regl to execute the command once for each object
-  draw([
-    { offset: [-1, -1], scale:0.2},
-    { offset: [-1, 0], scale:0.2},
-    { offset: [-1, 1], scale:0.2},
-    { offset: [0, -1], scale:0.2},
-    { offset: [0, 0], scale:0.2},
-    { offset: [0, 1], scale:0.2},
-    { offset: [1, -1], scale:0.2},
-    { offset: [1, 0], scale:0.2},
-    { offset: [1, 1], scale:0.2}
-  ])
+  draw(TheOrbManager.render())
 })
 
 }
