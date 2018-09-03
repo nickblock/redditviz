b26be8e6ee30c21f9eff3f6b4b6f5bbd61180dfd
Nick Block <nickblock@hotmail.com>
2018-09-03 19:11:35 +0100

    colors and border, poor res

--- public/batch.js
+++ public/batch.js
@@ -1,12 +1,21 @@
 /*
   tags: basic
-
+  
   <p>This example demonstrates how to use batch mode commands</p>
+  
+  <p> To use a command in batch mode, we pass in an array of objects.  Then
+  the command is executed once for each object in the array. </p>
+  */
+ 
+ var screenSize = [1.0, 1.0];
 
-<p> To use a command in batch mode, we pass in an array of objects.  Then
- the command is executed once for each object in the array. </p>
-*/
-
+ var colors = [
+    [44/255.0, 19/255.0, 32/255.0, 1.0],
+    [95/255.0, 75/255.0, 102/255.0, 1.0],
+    [167/255.0, 173/255.0, 198/255.0, 1.0],
+    [135/255.0, 151/255.0, 175/255.0, 1.0],
+    [86/255.0, 102/255.0, 122/255.0, 1.0],
+];
 
 var OrbManager = function() {
     
@@ -21,8 +30,8 @@ OrbManager.prototype = {
         var count = data.length < chart_item_max ? data.length : chart_item_max;
         for(var i=1; i<count; i++) {
             this.orbs.push({
-                x: i,
-                y: Math.floor(Math.random() * 10),
+                x: i * (screenSize[0]/count),
+                y: Math.floor(Math.random() * screenSize[1]),
                 r: data[i].count,
                 l: data[i].name
             });
@@ -41,14 +50,14 @@ OrbManager.prototype = {
 var TheOrbManager = new OrbManager();
 
 var circlePoints = 200;
-var screenSize = [1.0, 1.0]
 
 var genCircleAttributes = function(r, p) {
-    var attributes = [0.0, 0.0];
+    var attributes = [0.0, 0.0, 0.0];
     var frac = (Math.PI*2.0)/p;
     for(var i=0; i<p; i++) {
         attributes.push(r * Math.cos(i * frac));
         attributes.push(r * Math.sin(i * frac));
+        attributes.push(1.0); //last element used as color-outline in shader
     }
     return attributes;
 } 
@@ -69,20 +78,29 @@ const draw = regl({
   frag: `
     precision mediump float;
     uniform vec4 color;
+    varying float outside;
     void main() {
-      gl_FragColor = color;
+        if(outside > 0.98) {
+            gl_FragColor = vec4(0,0,0,1.0);
+        }
+        else {
+            gl_FragColor = color;
+        }
     }`,
 
   vert: `
     precision mediump float;
-    attribute vec2 position;
+    attribute vec3 position;
     uniform float scale;
     uniform vec2 offset;
     uniform vec2 screenSize;
+    varying float outside;
     void main() {
         vec2 pos = (vec2(position.x, position.y) * scale) + offset;
         pos = pos / screenSize;
-      gl_Position = vec4(pos, 0.0, 1.0);
+        pos = (pos * 2.0) - vec2(1.0);
+        gl_Position = vec4(pos, 0.0, 1.0);
+        outside = position.z;
     }`,
 
   attributes: {
@@ -93,12 +111,7 @@ const draw = regl({
 
   uniforms: {
     // the batchId parameter gives the index of the command
-    color: ({tick}, props, batchId) => [
-      Math.sin(0.02 * ((0.1 + Math.sin(batchId)) * tick + 3.0 * batchId)),
-      Math.cos(0.02 * (0.02 * tick + 0.1 * batchId)),
-      Math.sin(0.02 * ((0.3 + Math.cos(2.0 * batchId)) * tick + 0.8 * batchId)),
-      1
-    ],
+    color: ({tick}, props, batchId) => colors[batchId%colors.length],
     offset: regl.prop('offset'),
     scale: regl.prop('scale'),
     screenSize: screenSize
@@ -114,7 +127,7 @@ const draw = regl({
 // Here we register a per-frame callback to draw the whole scene
 regl.frame(function () {
   regl.clear({
-    color: [0, 0, 0, 1]
+    color: [0.7, 0.7, 0.7, 1]
   })
 
   // This tells regl to execute the command once for each object
