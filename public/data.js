cd9302bf47a72dbb2f1e20bb719d2d3d0ba753ce
Nick Block <nickblock@hotmail.com>
2018-09-08 17:18:30 +0100

    attempting to implement springs

--- public/data.js
+++ public/data.js
@@ -24,18 +24,91 @@ var data_fetch = async function(search) {
     });
 }
 
-var Engine = Matter.Engine,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
 // create an engine
 var physicsEngine = Matter.Engine.create();
 physicsEngine.world.gravity.scale = 0.0;
+
+var size_scale = 0.1;
+
+var Orb = function(data, primary) {
+    var xpos, ypos;
+    if(primary) {
+        xpos = screenSize[0]/2.0;
+        ypos = screenSize[0]/2.0;
+    }
+    else {
+        xpos = Math.random() * screenSize[0];
+        ypos = Math.random() * screenSize[1];
+    }
+    var body = Matter.Bodies.circle(
+        xpos, ypos,
+        data.count * size_scale
+    )
+    Matter.World.add(physicsEngine.world, body);
+    
+    this.body = body;
+    this.name = data.name;
+    this.subs = undefined;
+    if(primary) {
+        this.subs = data;
+    }
+}
+Orb.prototype = {
+    get_mutual_attraction: function(otherSub) {
+        var other_attr = otherSub.get_attraction(this.name);
+        if(other_attr !== undefined) {
+            var attr = this.get_attraction(otherSub.name);
+            if(attr !== undefined) {
+                var mutual = Math.min(other_attr, attr);
+                mutual = Math.max(0, mutual - Math.abs(other_attr, attr));
+
+                return mutual;
+            }
+        }
+        return undefined;
+    },
+    get_attraction: function(sub_name) {
+        if(this.subs == undefined) {
+            return undefined;
+        }
+        var v = 0;
+        for(var i=0; i<this.subs.length; i++) {
+            if(this.subs[i].namw == sub_name) {
+                v = this.subs[i].count;
+                break;
+            }
+        }
+        return v;
+    }
+
+}
+
+var Spring = function(bodyA, bodyB, length, strength) {
+    this.bodyA = bodyA;
+    this.bodyB = bodyB;
+    this.length = length;
+    this.strength = strength;
+}
+Spring.prototype = {
+    update: function() {
+        var v = {
+            x: this.bodyA.position.x - this.bodyB.position.x,
+            y: this.bodyA.position.y - this.bodyB.position.y
+        }
+        var l = Math.sqrt(v.x*v.x + v.y*v.y);
+        var f = (this.length - l) * this.strength;
+
+        Matter.Body.applyForce(this.bodyA, this.bodyB.position, f);
+        Matter.Body.applyForce(this.bodyB, this.bodyA.position, f);
+    }
+}
 
 var OrbManager = function(fnc) {
     
     this.orbs = {};
     this.maxradius = 0.0;
     this.display_message_func;
+    this.springs = []
 }
 OrbManager.prototype = {
 
@@ -58,36 +131,62 @@ OrbManager.prototype = {
             }
         }
     },
+    fetch_subs: async function(orb) {
+
+        try {
+            let resp = await data_fetch("r/" + orb.name);
+            if(resp.data) {
+                orb.subs = resp.data;
+
+                for(otherOrb of Object.values(this.orbs)) {
+                    if(otherOrb.name == orb.name || otherOrb.subs == undefined) continue;
+
+                    var ma = orb.get_mutual_attraction(otherOrb);
+                    if(ma != undefined) {
+                        // var constraint = Matter.Constraint.create({
+                        //     bodyA: orb.body,
+                        //     bodyB: otherOrb.body,
+                        //     length: (screenSize[0]/5.0) - ma,
+                        //     damping: 0.00001,
+                        //     siffnesss: 0.00001
+                        // });
+                        // Matter.World.addConstraint(physicsEngine.world, constraint);
+                        var s = new Spring(orb.body, otherOrb.body, (screenSize[0]/5.0) - ma, 0.000001);
+                        s.update();
+                        this.springs.push(s);
+                    }
+                }
+            }
+        }
+        catch(err) {
+            console.log(err);
+        }
+    },
 
     center: function(name, data) {
         
         this.orbs = {};
-        var count = Math.min(data.length, chart_item_max);
+        var count = Math.min(data.length, 4);
         for(var i=1; i<count; i++) {
-
-            var dataItem = data[i];
-            var body = Bodies.circle(
-                i * (screenSize[0]/count),
-                Math.random() * screenSize[1],
-                data[i].count
-            )
-            World.add(physicsEngine.world, body);
-            this.orbs[data[i].name] = body;
-            if(data[i].count > this.maxradius) {
-                this.maxradius = data[i].count;
+            var isPrimary = data[i].name == name;
+            var orb = new Orb(data[i], isPrimary);
+            if(!isPrimary) {
+                this.fetch_subs(orb);
             }
+            this.orbs[data[i].name] = orb;
         }
     },
-    append: function(data) {
-
-    },
     render: function() {
+
+        // for(var i=0; i<this.springs.length; i++) {
+        //     this.springs[i].update();
+        // }
         
-        Engine.update(physicsEngine, 1000 / 60);
+        Matter.Engine.update(physicsEngine, 1000 / 60);
 
         var drawArray = [];
         for(let orb of Object.values(this.orbs)) {
-            drawArray.push({offset:[orb.position.x, orb.position.y], scale:orb.circleRadius});
+            drawArray.push({offset:[orb.body.position.x, orb.body.position.y], scale:orb.body.circleRadius});
         }
         return drawArray;
     }
